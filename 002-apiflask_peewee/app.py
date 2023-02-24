ade797158d89fcd8ae6f70c70c61f710ed498edc
yannanxiu <ynanxiu@qq.com>
2023-02-24 16:48:06 +0800

    refactor: 重构 apiflask_peewee 代码

--- 002-apiflask_peewee/app.py
+++ 002-apiflask_peewee/app.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 from peewee import CharField,ForeignKeyField,TextField,DateTimeField
 from playhouse.shortcuts import model_to_dict
 
@@ -21,6 +22,7 @@ class FlaskDB(_FlaskDB):
 
 
 SECRET_KEY = 'secret-key'
+# DATABASE = 'sqlite:///db.sqlite3'
 DATABASE = 'mysql://root:Bingo@123@192.168.42.8:3306/apiflask'
 
 # If we want to exclude particular views from the automatic connection
@@ -42,17 +44,24 @@ app.config['BASE_RESPONSE_SCHEMA'] = BaseResponse
 db_wrapper = FlaskDB(app)
 
 
-class User(db_wrapper.Model):
+class BaseModel(db_wrapper.Model):
+
+    def to_dict(self):
+        return model_to_dict(self, backrefs=True)
+
+    def to_json(self):
+        data = self.to_dict()
+        return json.dumps(data, default=str)
+
+
+class User(BaseModel):
     username = CharField(unique=True)
 
     class Meta:
         table_name = 'user'
 
-    def to_dict(self):
-        return model_to_dict(self, backrefs=True)
 
-
-class Tweet(db_wrapper.Model):
+class Tweet(BaseModel):
     user = ForeignKeyField(User, backref='tweets')
     content = TextField()
     timestamp = DateTimeField(default=datetime.datetime.now)
@@ -90,12 +99,55 @@ def get_user_list():
 @app.output(UserOut)
 def get_user(id):
     instance = User.select().where(User.id==id).first()
-    print(instance.to_dict())
     if instance:
-        return {
-            'code': 0,
-            'data': instance.to_dict()
+        print(instance.to_dict())
+        if instance:
+            return {
+                'code': 0,
+                'data': instance.to_dict()
+            }
+    return {
+        'code': 1
+    }
+
+
+
+class TweetOut(Schema):
+    id = fields.Integer()
+    content = fields.String()
+    timestamp = fields.DateTime()
+
+
+class TweetListOut(Schema):
+    records = fields.List(fields.Nested(TweetOut))
+    total = fields.Integer()
+
+
+@app.get('/api/v1/tweet')
+@app.output(TweetListOut)
+def get_tweet_list():
+    paginated_query = PaginatedQuery(Tweet)
+    object_list = paginated_query.get_object_list()
+    total = paginated_query.get_object_total()
+    return {
+        'code': 0,
+        'data': {
+            'total': total,
+            'records': object_list
         }
+    }
+
+@app.get('/api/v1/tweet/<int:id>')
+@app.output(TweetOut)
+def get_tweet(id):
+    instance = Tweet.select().where(Tweet.id==id).first()
+    if instance:
+        print(instance.to_json())
+        if instance:
+            return {
+                'code': 0,
+                'data': instance.to_dict()
+            }
     return {
         'code': 1
     }
