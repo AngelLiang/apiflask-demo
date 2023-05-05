67b4786d64ce0581d0adb3e941e9cb49569e1bc1
yannanxiu <ynanxiu@qq.com>
2023-05-05 18:10:16 +0800

    refactor: 更新下载文件示例

--- 004-apiflask_download_file/app.py
+++ 004-apiflask_download_file/app.py
@@ -11,14 +11,70 @@ app = APIFlask(__name__, title='接口文档', version='1.0')
 app.config.from_object(__name__)
 
 
-@app.get('/downloadFile')
-def download_file():
+@app.get('/downloadJson')
+def download_json():
     export_data = [{'hello': 'world'}]
     f = BytesIO()
     data = json.dumps(export_data, ensure_ascii=False, default=str)
     f.write(data.encode())
     f.seek(0)
     return send_file(f, as_attachment=True, download_name='file.json')
+
+
+class Area:
+    def __init__(self,id,name) -> None:
+        self.id=0
+        self.name=''
+
+    def __str__(self) -> str:
+        return self.name
+
+class User:
+
+    def __init__(self,id,name,phone,area) -> None:
+        self.id=id
+        self.name=name
+        self.phone=phone
+        self.area=area
+
+
+def export_excel(records):
+    import xlwt
+    from io import BytesIO
+
+    wb = xlwt.Workbook()
+    sheet = wb.add_sheet('sheet1')
+    colnames = ('ID', '姓名', '手机号码', '区域')
+    for index, name in enumerate(colnames):
+        sheet.write(0, index, name)
+    props = ('id', 'name', 'phone', 'area')
+    for row, instance in enumerate(records):
+        for col, prop in enumerate(props):
+            if prop == 'area':
+                value = instance.area
+                if isinstance(value, Area) and value:
+                    value = str(value)
+                sheet.write(row + 1, col, value)
+                continue
+
+            value = getattr(instance, prop, '')
+            sheet.write(row + 1, col, value)
+
+    buffer = BytesIO()
+    wb.save(buffer)
+    buffer.seek(0)
+    return buffer
+
+@app.get('/downloadExcel')
+def download_excel():
+    area = Area(1,'area')
+    records = [
+        User(1,'user01','158', area),
+        User(2,'user02','158', area),
+        User(3,'user03','158', area)
+    ]
+    filedata = export_excel(records)
+    return send_file(filedata, as_attachment=True, download_name='file.xls')
 
 
 if __name__ == "__main__":
